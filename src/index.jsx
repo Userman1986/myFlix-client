e03a07afe4b9a4208d50df7a29fd0f351265c487
Userman1986 <e.samonin1986@gmail.com>
2023-08-25 15:09:49 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
 import { createRoot } from 'react-dom/client';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import { createRoot } from "react-dom/client";
 
 import { MainView } from "./components/main-view/main-view";
+
+ReactDOM.render(<MainView />, document.getElementById('root'));
 
 import "./index.scss";
 const App = () => {
