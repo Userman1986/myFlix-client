1400f498950f16a77002568e16eb208feca178ae
Userman1986 <e.samonin1986@gmail.com>
2023-09-08 08:33:28 +0200

    rr

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -63,43 +63,43 @@ export const MainView = ({ apiUrl }) => {
   };
 
   return (
-    <div>
+    <Container>
       {user ? (
         <div>
-          <button onClick={handleLogout}>Logout</button>
-          {movies.map((movie) => (
-            <MovieCard
-              key={movie._id}
-              movie={movie}
-              onMovieClick={() => {
-                setSelectedMovie(movie);
-              }}
-            />
-          ))}
-          {selectedMovie && (
-            <MovieView
-              movie={selectedMovie}
-              onBackClick={() => setSelectedMovie(null)}
-            />
-          )}
+          <Button variant="danger" onClick={handleLogout}>
+            Logout
+          </Button>
+          <Row>
+            {movies.map((movie) => (
+              <Col key={movie._id} sm={6} md={4} lg={3}>
+                <Link to={`/movies/${movie._id}`}>
+                  <MovieCard movie={movie} />
+                </Link>
+              </Col>
+            ))}
+          </Row>
         </div>
       ) : (
-        <div>
-          <LoginView onLoggedIn={(user, token) => {
-            setUser(user);
-            setToken(token);
-          }} />
-          or
-
-          {showSignup ? (
-  <SignupView onSignup={handleSignup} />
-) : (
-  <button onClick={toggleSignup}>Signup</button>
-)}
+        <div className="login-container">
+          <LoginView
+            onLoggedIn={(user, token) => {
+              setUser(user);
+              setToken(token);
+            }}
+          />
+          <div className="signup-form">
+            {showSignup ? (
+              <SignupView onSignup={handleSignup} />
+            ) : (
+              <Button className="signup-button" onClick={toggleSignup}>
+                Signup
+              </Button>
+            )}
+          </div>
         </div>
       )}
-    </div>
+    </Container>
   );
-}
+};
 
 export default MainView;
