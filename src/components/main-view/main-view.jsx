3315129772eba28358d09caa67ca3820e5b90c24
Userman1986 <e.samonin1986@gmail.com>
2023-09-08 08:23:42 +0200

    41

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,27 +3,13 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import SignupView from '../signup-view/signup-view';
-import { Row, Col, Container, Button } from "react-bootstrap";
-import "../../dist/index.css";
 
 export const MainView = ({ apiUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(localStorage.getItem('user') || null);
   const [token, setToken] = useState(localStorage.getItem('token') || null);
   const [showSignup, setShowSignup] = useState(false);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const [expandedMovie, setExpandedMovie] = useState(null); // Track expanded movie
-=======
-  const [expandedMovie, setExpandedMovie] = useState(null);
->>>>>>> parent of b9d9434 (6543)
-=======
-  const [expandedMovie, setExpandedMovie] = useState(null);
->>>>>>> parent of b9d9434 (6543)
-=======
-  const [expandedMovie, setExpandedMovie] = useState(null);
->>>>>>> parent of b9d9434 (6543)
 
   useEffect(() => {
     if (token) {
@@ -70,97 +56,50 @@ export const MainView = ({ apiUrl }) => {
   };
 
   const handleSignup = () => {
-    
-  };
 
+  };
   const toggleSignup = () => {
     setShowSignup(!showSignup);
   };
 
-  const handleMovieCardClick = (movie) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    setSelectedMovie(movie); 
-=======
-    setExpandedMovie(movie); 
->>>>>>> parent of b9d9434 (6543)
-=======
-    setExpandedMovie(movie); 
->>>>>>> parent of b9d9434 (6543)
-=======
-    setExpandedMovie(movie); 
->>>>>>> parent of b9d9434 (6543)
-  };
-
-  const handleBackClick = () => {
-    setExpandedMovie(null);
-  };
-
   return (
-    <Container>
+    <div>
       {user ? (
         <div>
-          <Button variant="danger" onClick={handleLogout}>
-            Logout
-          </Button>
-          <Row>
-            {movies.map((movie) => (
-              <Col key={movie._id} sm={6} md={4} lg={3}>
-                <MovieCard
-                  movie={movie}
-                  onMovieClick={() => handleMovieCardClick(movie)}
-                />
-              </Col>
-            ))}
-          </Row>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+          <button onClick={handleLogout}>Logout</button>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={() => {
+                setSelectedMovie(movie);
+              }}
+            />
+          ))}
           {selectedMovie && (
-  <MovieView
-    movie={selectedMovie}
-    onBackClick={handleBackClick}
-  />
-)}
-=======
-=======
->>>>>>> parent of b9d9434 (6543)
-=======
->>>>>>> parent of b9d9434 (6543)
-          {expandedMovie && (
             <MovieView
-              movie={expandedMovie}
-              onBackClick={handleBackClick}
+              movie={selectedMovie}
+              onBackClick={() => setSelectedMovie(null)}
             />
           )}
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of b9d9434 (6543)
-=======
->>>>>>> parent of b9d9434 (6543)
-=======
->>>>>>> parent of b9d9434 (6543)
         </div>
       ) : (
-        <div className="login-container">
-          <LoginView
-            onLoggedIn={(user, token) => {
-              setUser(user);
-              setToken(token);
-            }}
-          />
-          <div className="signup-form">
-            {showSignup ? (
-              <SignupView onSignup={handleSignup} />
-            ) : (
-              <Button className="signup-button" onClick={toggleSignup}>
-                Signup
-              </Button>
-            )}
-          </div>
+        <div>
+          <LoginView onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }} />
+          or
+
+          {showSignup ? (
+  <SignupView onSignup={handleSignup} />
+) : (
+  <button onClick={toggleSignup}>Signup</button>
+)}
         </div>
       )}
-    </Container>
+    </div>
   );
-};
+}
+
+export default MainView;
